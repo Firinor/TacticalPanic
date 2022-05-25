9d7827717a7113643910392e285ad0825ffc0cbb
Firinor <tarmz@rambler.ru>
2022-05-25 12:01:57 +0300

    csv

--- _AnyScene/CSVOperator.cs
+++ _AnyScene/CSVOperator.cs
@@ -1,19 +1,17 @@
 using System.IO;
 using UnityEngine;
-using UnityEngine.Windows;
-using Directory = UnityEngine.Windows.Directory;
 
 public class CSVOperator : MonoBehaviour
 {
-    [SerializeField]
     private string path;
 
     void Start()
     {
-        path = Application.dataPath + "/Database/Unitdata.csv";
+        path = Application.dataPath + "/Database/UnitData.csv";
 
-        string AllText = SaveManager.Load<string>(path);
+        string[] AllText = File.ReadAllLines(path);
 
-        Debug.Log(AllText);
+        foreach (string text in AllText)
+            Debug.Log(text);
     }
 }
