90bac856f7f70e7ac6e63dbaa121b740661c4e76
Firinor <tarmz@rambler.ru>
2022-07-13 16:15:30 +0300

    Dialogs good system

--- Dialogs/DialogOperator.cs
+++ Dialogs/DialogOperator.cs
@@ -29,10 +29,13 @@ public class DialogOperator : SinglBehaviour<DialogOperator>
     [SerializeField]
     private Image nextArrow;
 
+    [SerializeField]
+    private DialogInformator dialog;
+
     private StringBuilder strindBuilder = new StringBuilder();
 
     private Dictionary<UnitInformator, SpeakerOperator> speakers = new Dictionary<UnitInformator, SpeakerOperator>();
-    private UnitInformator activeSpeaker;
+    private SpeakerOperator activeSpeaker = null;
 
     public static GameObject Left { get { return instance.leftSpeaker; } }
     public static GameObject Center { get { return instance.centerSpeaker; } }
@@ -43,31 +46,92 @@ public class DialogOperator : SinglBehaviour<DialogOperator>
     void Awake()
     {
         SingletoneCheck<DialogOperator>(this);
+        StartCoroutineDialog(dialog);
+    }
+
+    public void StartCoroutineDialog(DialogInformator dialog)
+    {
+        StartCoroutine(StartDialog(dialog));
     }
 
     public IEnumerator StartDialog(DialogInformator dialog)
     {
-        bool AllLetters = false;
-        strindBuilder.Clear();
+        ClearAllSpeakers();
+        bool NextInput = false;
         for (int i = 0; i < dialog.Length; i++)
         {
-            nextArrow.enabled = false;
-            AllLetters = false;
-            text.text = "";
             DialogInformator.SpeakersPhrase speakersPhrase = dialog[i];
+
+            if(speakersPhrase.Position == StagePosition.Off)
+            {
+                LeaveTheStage(speakersPhrase.Speaker);
+                continue;
+            }
+
+            strindBuilder.Clear();
+            nextArrow.enabled = false;
+            NextInput = false;
+            text.text = strindBuilder.ToString();
+            
             SetBackground(speakersPhrase.background);
             UnitInformator speaker = CheckSpeaker(speakersPhrase);
             SetActiveSpeaker(speaker);
-            while (!AllLetters)
+            string phrase = speakersPhrase.text;
+            for (int j = 0; j < phrase.Length; j++)
             {
-
+                strindBuilder.Append(speakersPhrase.text[j]);
+                text.text = strindBuilder.ToString();
                 yield return new WaitForSeconds(lettersDelay);
             }
-                
-
-
+            while (!NextInput)
+            {
+                if (Input.anyKeyDown) { NextInput = true; }
+                yield return null;
+            }
         }
-        yield break;
+        EndOfDialog();
+    }
+
+    private void LeaveTheStage(UnitInformator speaker)
+    {
+        SpeakerOperator speakerOperator = null;
+
+        if (speakers.ContainsKey(speaker))
+        {
+            speakerOperator = speakers[speaker];
+            speakers.Remove(speaker);
+        }
+
+        if (speakerOperator != null)
+            Destroy(speakerOperator.gameObject);
+    }
+
+    private void EndOfDialog()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void ClearAllSpeakers()
+    {
+        speakers.Clear();
+
+        List<SpeakerOperator> gameObjectToDelete = new List<SpeakerOperator>();
+        AllSpeakersIn(gameObjectToDelete, leftSpeaker);
+        AllSpeakersIn(gameObjectToDelete, centerSpeaker);
+        AllSpeakersIn(gameObjectToDelete, rightSpeaker);
+
+        foreach(SpeakerOperator speaker in gameObjectToDelete)
+        {
+            Destroy(speaker.gameObject);
+        }
+    }
+
+    private void AllSpeakersIn(List<SpeakerOperator> gameObjectToDelete, GameObject side)
+    {
+        foreach (SpeakerOperator gameObject in side.GetComponentsInChildren<SpeakerOperator>())
+        {
+            gameObjectToDelete.Add(gameObject);
+        }
     }
 
     private UnitInformator CheckSpeaker(DialogInformator.SpeakersPhrase speakersPhrase)
@@ -75,33 +139,39 @@ public class DialogOperator : SinglBehaviour<DialogOperator>
         UnitInformator speaker = speakersPhrase.Speaker;
         if (!speakers.ContainsKey(speaker))
         {
-            GameObject newSpeaker = Instantiate(speakerPrefab, GetParent(speakersPhrase.Position));
+            GameObject newSpeaker = Instantiate(speakerPrefab, GetSpeakerParent(speakersPhrase.Position));
             speakers.Add(speakersPhrase.Speaker, newSpeaker.GetComponent<SpeakerOperator>());
+            speakers[speaker].SetImage(speaker);
         }
         else
         {
-            speakers[speaker].transform.SetParent(GetParent(speakersPhrase.Position));
+            speakers[speaker].transform.SetParent(GetSpeakerParent(speakersPhrase.Position));
         }
+
+        speakerName.text = speaker.Name;
 
         return speaker;
     }
 
     private void SetBackground(Sprite background)
     {
-        throw new NotImplementedException();
+        this.background.sprite = background;
     }
 
     private void SetActiveSpeaker(UnitInformator speaker)
     {
-        if (activeSpeaker == speaker)
+        if ( activeSpeaker != null 
+            && activeSpeaker == speakers[speaker])
             return;
 
-        speakers[speaker].ToTheWayside();
+        if (activeSpeaker != null)
+            activeSpeaker.ToTheBackground();
 
-        activeSpeaker = speaker;
+        activeSpeaker = speakers[speaker];
+        activeSpeaker.ToTheForeground();
     }
 
-    private Transform GetParent(StagePosition position)
+    private Transform GetSpeakerParent(StagePosition position)
     {
         return position switch
         {
--- Dialogs/SpeakerOperator.cs
+++ Dialogs/SpeakerOperator.cs
@@ -2,11 +2,33 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeakerOperator : MonoBehaviour
 {
-    internal void ToTheWayside()
+    [SerializeField]
+    private Image image;
+    [SerializeField]
+    private Transform imageTransform;
+    [SerializeField]
+    private Color speakerOnBackgroundColor;
+    [SerializeField]
+    private Vector3 scaleOnBackground;
+
+    internal void SetImage(UnitInformator speaker)
     {
-        throw new NotImplementedException();
+        image.sprite = speaker.unitSprite;
+    }
+
+    public void ToTheBackground()
+    {
+        image.color = speakerOnBackgroundColor;
+        imageTransform.localScale = scaleOnBackground;
+    }
+
+    public void ToTheForeground()
+    {
+        image.color = Color.white;
+        imageTransform.localScale = Vector3.one;
     }
 }
