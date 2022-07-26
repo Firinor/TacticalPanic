f905f1ca43ca40ac508aed4ccbbf202592cc490b
Firinor <tarmz@rambler.ru>
2022-07-26 22:19:59 +0300

    VFX on slash

---
+++ Units/UnitVFXOperator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace TacticalPanicCode
+{
+    public class UnitVFXOperator : MonoBehaviour
+    {
+        [SerializeField]
+        private VisualEffect visualEffect;
+        [SerializeField]
+        [Range (0, 359)]
+        private int loocAtAngle;
+        [SerializeField]
+        private float slashSpeed = 10;
+
+        void Awake()
+        {
+            SetAngle();
+            SetSpeed();
+        }
+
+        private void SetAngle(int loocAtAngle)
+        {
+            this.loocAtAngle = loocAtAngle;
+            SetAngle();
+        }
+        private void SetAngle()
+        {
+            visualEffect.SetFloat("SlashSpeed", loocAtAngle);
+        }
+
+        private void SetSpeed(float slashSpeed)
+        {
+            this.slashSpeed = slashSpeed;
+            SetSpeed();
+        }
+        private void SetSpeed()
+            {
+                visualEffect.SetFloat("SlashSpeed", slashSpeed);
+        }
+    }
+}
